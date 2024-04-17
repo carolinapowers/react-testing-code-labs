fd9479ffc5e677c8f0a12a5b7cf50348193323b2
carolinapowers <carolinapoloni@gmail.com>
2024-04-17 16:57:34 -0400

    Uses jest-expect-message and modify reporter to remove the package's 'Custom message' string to print only users msg

--- src/test-utils/helpers.tsx
+++ src/test-utils/helpers.tsx
@@ -1,9 +1,8 @@
 import fs from "fs";
 import path from "path";
-import parse5 from "parse5";
 import assert from "assert";
 
-const readFile = (filePath: string): string => {
+export const readFile = (filePath: string): string => {
   let file: string;
   try {
     file = fs.readFileSync(path.join(process.cwd(), filePath), "utf8");
@@ -14,41 +13,5 @@ const readFile = (filePath: string): string => {
   return file;
 };
 
-const parseFile = (file: string): any => {
-  const doc = parse5.parseFragment(file.replace(/\n/g, ""), {
-    locationInfo: true
-  });
-  const nodes = doc.childNodes;
-
-  return nodes;
-};
-
-export { readFile, parseFile };
-
-// const fs = require("fs");
-// const path = require("path");
-// const parse5 = require("parse5");
-
-// const readFile = function(filePath) {
-//   let file;
-//   try {
-//     file = fs.readFileSync(path.join(process.cwd(), filePath), "utf8");
-//   } catch (e) {
-//     assert(false, "The BookForm.vue file does not exist");
-//   }
-
-//   return file;
-// };
-
-// const parseFile = function(file) {
-//   const doc = parse5.parseFragment(file.replace(/\n/g, ""), {
-//     locationInfo: true
-//   });
-//   const nodes = doc.childNodes;
-
-//   return nodes;
-// };
 
 
-// module.exports.readFile = readFile;
-// module.exports.parseFile = parseFile;
--- src/tests/reporter.js
+++ src/tests/reporter.js
@@ -4,25 +4,21 @@ class Reporter {
     this._options = options;
   }
 
-splitString(str = '', size) {
-   
-   const chunks = str.match(regex);
-   return chunks;
-}
-
   onRunComplete(_contexts, results) {
     const { testResults } = results;
 
     if (testResults.length > 0) {
-
       const { failureMessage } = testResults[0];
+      
       if (failureMessage != null) {
-        const message =
-          failureMessage
-          .split(/\n\n/)[1]
-          .trim()
-          .match(/\S.{1,68}\S(?= |$)/g).join("\n");
-        console.log(message, "\n<failed>");
+        const message = failureMessage
+          .split(/\n\n/)[1] // split the message and get the message
+          .split(/\n/)[1] // split the message and get only the custom message
+          .trim() // remove leading and trailing spaces
+          .match(/\S.{1,68}\S(?= |$)/g) // match and return an array of strings that fit the regex pattern
+          .join("\n"); // join the array of strings with a newline character
+
+        console.log(message); 
       }
     }
   }
