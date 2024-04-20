67d03c1c3d25246e796dd7f6eb1f6d12ddd4cab6
carolinapowers <carolinapoloni@gmail.com>
2024-04-19 23:46:07 -0400

    Change file extension to reflect TS in it

---
+++ src/tests/1.spec.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('renders learn react link with mint @1-2', () => {
+  render(<App />);
+  const linkElement = screen.queryByText(/learn react/i);
+  expect(linkElement, "My not so great message").not.toBeInTheDocument();
+});
+
