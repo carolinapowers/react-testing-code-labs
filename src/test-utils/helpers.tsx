793bb107ee62ffcc1253e1a6f135da52373ce3dd
carolinapowers <carolinapoloni@gmail.com>
2024-04-20 15:58:59 -0400

    Add log to file helper

--- src/test-utils/helpers.tsx
+++ src/test-utils/helpers.tsx
@@ -4,9 +4,13 @@ import assert from "assert";
 
 export const readFile = (filePath: string): string => {
   let file: string;
+
   try {
     file = fs.readFileSync(path.join(process.cwd(), filePath), "utf8");
+    console.log(`Test file exists:`, fs.existsSync(file));
+
   } catch (e) {
+
     assert(false, "The expected file does not exist");
   }
 
