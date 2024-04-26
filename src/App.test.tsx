2e0a42db22206f3086bc9eaaf48b2b81945b6a4c
carolinapowers <carolinapoloni@gmail.com>
2024-04-25 22:22:54 -0400

    Lowercase file names

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './app';
 
 test('renders learn react link @1-1', () => {
   render(<App />);
--- src/tests/1.spec.tsx
+++ src/tests/1.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from '../App';
+import App from '../app';
 
 test('renders learn react link with mint @1-2', () => {
   render(<App />);
