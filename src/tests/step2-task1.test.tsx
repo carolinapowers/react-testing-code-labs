9f0244184a909bf1437260097130eb99581b75d1
carolinapowers <carolinapoloni@gmail.com>
2024-05-05 22:33:06 -0400

    Add tests to test:ignore script

--- src/tests/step2-task1.test.tsx
+++ src/tests/step2-task1.test.tsx
@@ -24,3 +24,14 @@ describe("Jest CLI task @2.2", () => {
     expect(scripts['test:ci'], 'Make sure to assign "CI=true npm test" to your `test:blind` script').toEqual('CI=true npm test');
   });
 });
+
+// npm run -s task -- --testNamePattern=@2.3
+describe("Jest CLI task @2.3", () => {
+  test('test:ignore should exist', () => {
+    expect(scripts['test:ignore'], 'Make sure you are creating the script named "test:ignore"').toBeDefined();
+  });
+
+  test("testPathIgnorePatterns='<rootDir>/src/tests/.*.test.tsx'should exist", () => {
+    expect(scripts['test:ignore'], `Make sure your package.json has an "test:ignore": "npm test -- --watchAll=false --testPathIgnorePatterns='<rootDir>/src/tests/.*.test.tsx'"`).toEqual("npm test -- --watchAll=false --testPathIgnorePatterns='<rootDir>/src/tests/.*.test.tsx'");
+  });
+});
